1b95867478f7bc4b33b739141b4707811796728e
Zulfiqar-Ali <52883632+zulfiqarAlibalti@users.noreply.github.com>
2022-04-12 17:24:29 +0500

    Adding functional programming

---
+++ hand-tracking/MyHandTrackingGame.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp
+import  time
+import HandTrackingModule as htm
+pTime = 0
+cTime = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    detector.findHands(img)
+    lmlist = detector.findPosition(img)
+    if len(lmlist) != 0:
+        print(lmlist[4])
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 255), 3)
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
--- hand-tracking/hand-tracking.py
+++ hand-tracking/hand-tracking.py
@@ -2,11 +2,16 @@ import cv2
 import mediapipe as mp
 import time
 
+import skimage.feature
+
 cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 
 while True:
@@ -17,10 +22,23 @@ while True:
 
     if results.multi_hand_landmarks:
         for handlms in results.multi_hand_landmarks:
+            for id,lm in enumerate(handlms.landmark):
+                #print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w),int(lm.y*h)
+                print(id,cx,cy)
+                if id==4:
+                    cv2.circle(img, (cx, cy), 30, (255,0,255),cv2.FILLED)
+
+
             mpDraw.draw_landmarks(img, handlms,mpHands.HAND_CONNECTIONS)
 
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
 
 
-
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,
+                (255,0,255),3)
     cv2.imshow("image",img)
     cv2.waitKey(1)
